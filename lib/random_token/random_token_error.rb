9aa055ba00333bf3f4705fe9e564c0f1114e8300
Sibevin Wang <sibevin@gmail.com>
2013-07-20 09:43:52 +0800

    Remove status attribute in RandomTokenError
    DESC:
    [Change]
    1. Remove status attribute in RandomTokenError
    The status is not used for now.

--- lib/random_token/random_token_error.rb
+++ lib/random_token/random_token_error.rb
@@ -6,38 +6,32 @@ module RandomToken
     # Errors used in RandomToken
     ERRORS = {
       :unknown_seed => {
-        :status => :failed,
         :value => 1,
         :msg => "Unknown given seed"
       },
       :not_support_case => {
-        :status => :failed,
         :value => 2,
         :msg => "Case feature is not supported in this case, but the case option is given."
       },
       :not_support_friendly => {
-        :status => :failed,
         :value => 3,
         :msg => "Friendly feature is not supported in this case, but the friendly option is true."
       },
       :false_friendly_with_given_mask => {
-        :status => :failed,
         :value => 4,
         :msg => "The mask is given but the friendly option is false."
       },
       :invalid_strf_pattern => {
-        :status => :failed,
         :value => 5,
         :msg => "The given pattern is invalid."
       },
       :invalid_gen_arg => {
-        :status => :failed,
         :value => 6,
         :msg => "The given arg is invalid."
       }
     }
 
-    attr_reader :code, :value, :msg, :info, :status
+    attr_reader :code, :value, :msg, :info
 
     # The RandomTokenError constructor.
     # @param error [Fixnum, String]
@@ -47,11 +41,9 @@ module RandomToken
     def initialize(error, info = {})
       @code = error
       @info = info
-      @status = :failed
       if ERRORS.keys.include?(error)
         @value = ERRORS[error][:value]
         @msg = ERRORS[error][:msg]
-        @status = ERRORS[error][:status]
       elsif error.class.name == 'String'
         @code = :internal
         @value = 90000
@@ -65,7 +57,7 @@ module RandomToken
 
     # Override the message method to show more information in RandomTokenError
     def message
-      "RandomTokenError(#{@code.to_s}): value = #{@value}, status = #{@status.to_s}, msg = #{@msg}, info = #{@info.inspect}"
+      "RandomTokenError(#{@code.to_s}): value = #{@value}, msg = #{@msg}, info = #{@info.inspect}"
     end
   end
 end
